7d3d3363ee2b041b4462988e7b110ab8ebda44d7
Atomic207 <elijah.rietmann@icloud.com>
2024-02-16 12:00:48 +0100

    js finished + tutorial end

--- scriptmain.js
+++ scriptmain.js
@@ -5,18 +5,40 @@ var score = document.getElementById("score");
 var game = document.getElementById("game");
 var counter = 0;
 
-blueCar.addEventListener("animationiteration",function(){
-    var random = ((Math.floor(Math.random()*3))*100)
-    blueCar.style.left = random+"px";
-    counter++;
-})
+blueCar.addEventListener("animationiteration", function () {
+  var random = Math.floor(Math.random() * 3) * 100;
+  blueCar.style.left = random + "px";
+  counter++;
+});
 
-window.addEventListener("keydown",function(e){
-    if(e.keyCode =="39"){
-        var redCarleft = parseInt(window.getComputedStyle(redCar).getPropertyValue("left"))
-        if(redCarleft<200){
-            redCar.style.left = (redCarleft+100)+"px";
-        }
-    };
-    if(e.keyCode =="37")
-})
+window.addEventListener("keydown", function (e) {
+  if (e.keyCode == "39") {
+    var redCarLeft = parseInt(
+      window.getComputedStyle(redCar).getPropertyValue("left")
+    );
+    if (redCarLeft < 200) {
+      redCar.style.left = redCarLeft + 100 + "px";
+    }
+  }
+  if (e.keyCode == "37") {
+    var redCarLeft = parseInt(
+      window.getComputedStyle(redCar).getPropertyValue("left")
+    );
+    if (redCarLeft > 0) {
+      redCar.style.left = redCarLeft - 100 + "px";
+    }
+  }
+});
+setInterval(function Gameover(){
+    var blueCarTop= parseInt(window.getComputedStyle(blueCar).getPropertyValue("top"));
+     var blueCarLeft= parseInt(window.getComputedStyle(blueCar).getPropertyValue("left"));
+     var redCarLeft= parseInt(window.getComputedStyle(redCar).getPropertyValue("left"));
+     if((blueCarLeft === redCarLeft)&&(blueCarTop>250)&&(blueCarTop<450)){
+        result.style.display ="block";
+        game.style.display ="none";
+        score.innerHTML =`Your score is ${counter}`;
+
+        counter = 0;
+
+     }
+ },10)
